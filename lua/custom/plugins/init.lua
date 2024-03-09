27736cb8f6dd4ddf735b63518696c269df2544b5
AlexCzar <AlexCzar@users.noreply.github.com>
2024-03-09 19:10:04 +0100

    copilot, ansible

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,46 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- custom config which piggybacks on the copilot extras in lazy.lua.
+  {
+    'zbirenbaum/copilot.lua',
+    enabled = true,
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+    },
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        filetypes = {
+          yaml = true,
+        },
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        suggestion = {
+          enabled = true,
+          -- use the built-in keymapping for "accept" (<M-l>)
+          auto_trigger = true,
+          accept = false, -- disable built-in keymapping
+        },
+      }
+
+      -- hide copilot suggestions when cmp menu is open
+      -- to prevent odd behavior/garbled up suggestions
+      local cmp_status_ok, cmp = pcall(require, 'cmp')
+      if cmp_status_ok then
+        cmp.event:on('menu_opened', function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+
+        cmp.event:on('menu_closed', function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
+    end,
+  },
+}
