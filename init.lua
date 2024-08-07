010717c00998a35e49c15ee46db42834d44bd983
AlexCzar <AlexCzar@users.noreply.github.com>
2024-08-07 19:26:21 +0200

    feat(keymaps) buf next/prev navigation

--- init.lua
+++ init.lua
@@ -190,6 +190,15 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- [[ czar's keymaps: start ]]
+-- Back/Forward for buffer navigation
+vim.keymap.set('n', '<D-Left>', ':bprev<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<D-Right>', ':bnext<CR>', { desc = 'Next buffer' })
+vim.keymap.set('i', '<D-Left>', ':bprev<CR>', { desc = 'Previous buffer' })
+vim.keymap.set('i', '<D-Right>', ':bnext<CR>', { desc = 'Next buffer' })
+
+-- [[ czar's keymaps: end ]]
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
