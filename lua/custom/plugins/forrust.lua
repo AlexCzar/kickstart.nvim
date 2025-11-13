6eec09d9db437b7093ed881bdceaf09737c703c8
AlexCzar <AlexCzar@users.noreply.github.com>
2025-11-13 21:38:15 +0100

    configure crates.nvim plugin

--- lua/custom/plugins/forrust.lua
+++ lua/custom/plugins/forrust.lua
@@ -1,21 +1,9 @@
 return {
   {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      {
-        'Saecki/crates.nvim',
-        event = { 'BufRead Cargo.toml' },
-        opts = {
-          completion = {
-            cmp = { enabled = true },
-          },
-        },
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, { name = 'crates' })
+    'Saecki/crates.nvim',
+    event = { 'BufRead Cargo.toml' },
+    config = function()
+      require('crates').setup {}
     end,
   },
 }
