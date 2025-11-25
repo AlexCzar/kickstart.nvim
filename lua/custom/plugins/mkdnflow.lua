c3fd953783c62cbc273d4afa7d2aa0b01fdb8fb7
AlexCzar <AlexCzar@users.noreply.github.com>
2025-11-25 14:20:12 +0100

    conf: mkdownflow

--- lua/custom/plugins/mkdnflow.lua
+++ lua/custom/plugins/mkdnflow.lua
@@ -3,13 +3,42 @@ return {
       'jakewvincent/mkdnflow.nvim',
       config = function()
          require('mkdnflow').setup {
-            -- Config goes here; leave blank for defaults
+            mappings = {
+               MkdnNewListItem = { 'i', '<CR>' }, -- smart Enter in lists
+            },
+            perspective = {
+               root_tell = '.jj',
+               priority = 'root',
+            },
             to_do = {
                symbols = { ' ', '-', 'x' },
                update_parents = true,
                not_started = ' ',
                in_progress = '-',
                complete = 'x',
+            },
+            new_file_template = {
+               use_template = true,
+               placeholders = {
+                  before = {
+                     date = function()
+                        return os.date '%Y-%m-%d'
+                     end,
+                  },
+               },
+               template = [[
+---
+title: {{ title }}
+created: {{ date }}
+tags: [journal, pure]
+---
+
+# {{ title }}
+
+### Tasks
+
+- [ ] 
+]],
             },
          }
       end,
