84ef5feec79b5b88ce2f124f6553d1ae17267dd3
AlexCzar <AlexCzar@users.noreply.github.com>
2026-05-23 13:25:44 +0200

    fix: nvim everpure color theme switching

--- lua/custom/plugins/everpure.lua
+++ lua/custom/plugins/everpure.lua
@@ -1,2 +1,13 @@
 -- Everpure is a local colorscheme in colors/everpure.lua
+if vim.v.servername == '' then
+  pcall(vim.fn.serverstart)
+end
+
+local kitty_theme = vim.fn.resolve(vim.fn.expand '~/.config/kitty/kitty.d/theme.conf')
+if kitty_theme:match 'everpure%-light%.conf$' then
+  vim.o.background = 'light'
+elseif kitty_theme:match 'everpure%-dark%.conf$' then
+  vim.o.background = 'dark'
+end
+
 vim.cmd.colorscheme 'everpure'
